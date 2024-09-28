1d0452f7655dc3650a311f0bb04613c0af95b9a9
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2024-09-28 13:49:56 +0530

    dex contract structs defined

--- contract/contracts/Lock.sol
+++ contract/contracts/Lock.sol
@@ -3,7 +3,29 @@ pragma solidity ^0.8.27;
 import "hardhat/console.sol";
 
 contract Lock {
-    function main() public {
-        
+    struct Token {
+        address owner;
+        string token_hash; // sha256 hash
+        string name;
+        string ticker;
+        string icon_url;
+        string description;
+        string telegram;
+        string x_dot_com;
+        string website_url;
+        bool graduation;
+        mapping (address=>int64) owners;
+        Token_State token_state;
+    }
+    struct Token_State {
+        int64 total_tokens;
+        int64 liquidity;
+    }
+    struct Position {
+        string position_type;
+        int64 amount;
+        Token_State initial_state;
+        int64 time_to_live;
+        address owner;
     }
 }
--- readme.md
+++ readme.md
@@ -11,5 +11,5 @@ npx hardhat node --fork https://neoxt4seed1.ngd.network --hostname 0.0.0.0
 ### To deploy the Lock.sol contract to the localhost network
 
 ```
-npx hardhat run scripts/deploy.js --network localhost 
+npx hardhat run scripts/deploy.js --network localhost
 ```

2487809fda9a3625ada714e636f8d660d6e39820
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2024-09-28 12:51:58 +0530

    pakojs daisyui and tailwind added

--- web.py
+++ web.py
@@ -5,6 +5,32 @@ import secrets_parser
 
 secrets=secrets_parser.parse("variables.txt")
 
+daisyui="<script>"+open("public/pako.js").read()+"</script>"+"""<script>
+    function decompressGzippedString(base64String) {
+        try {
+            const binaryString = atob(base64String);
+            const len = binaryString.length;
+            const bytes = new Uint8Array(len);
+            for (let i = 0; i < len; i++) {
+                bytes[i] = binaryString.charCodeAt(i);
+            }
+            const decompressedData = pako.inflate(bytes, { to: 'string' });
+            return decompressedData;
+        } catch (err) {
+            console.error('An error occurred while decompressing the string:', err);
+            return null;
+        }
+    }
+    """+f"""
+    var daisycss="{open("public/daisyui.b64").read()}";
+    var style=document.createElement("style");
+    style.textContent=decompressGzippedString(daisycss);
+    document.head.appendChild(style);
+    </script>
+    """
+
+tailwind="<script>"+open("public/tailwind.js").read()+"</script>"
+
 app = Flask(__name__)
 
 @app.get("/")
